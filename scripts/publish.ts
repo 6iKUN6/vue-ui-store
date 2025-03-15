944c2fef038d7aa8889856ad3e8abe4e7ed5e176
Sparkle <2705263765@qq.com>
2025-03-15 20:16:34 +0800

    feat: 优化发包更新版本号流程

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,14 +1,27 @@
 import { series } from 'gulp';
-// import fs from 'fs';
-// import path from 'path';
+import fs from 'fs';
+import path from 'path';
 
-import { distPath } from '../build/utils/paths';
+import { distPath, rootPath } from '../build/utils/paths';
 import { run } from '../build/utils/run';
 import { createPkg } from './createPkgJson';
 import { buildChecker } from './builtChecker';
 
 export const publish = async () => {
-  await run('release-it', distPath).then(() => {});
+  await run('release-it', distPath).then(() => {
+    //更新根目录下的package.json的版本号
+    const distPkg = JSON.parse(
+      fs.readFileSync(path.resolve(distPath, 'package.json'), 'utf-8')
+    );
+    const rootPkg = JSON.parse(
+      fs.readFileSync(path.resolve(rootPath, 'package.json'), 'utf-8')
+    );
+    rootPkg.version = distPkg.version;
+    fs.writeFileSync(
+      path.resolve(rootPath, 'package.json'),
+      JSON.stringify(rootPkg, null, 2)
+    );
+  });
 };
 
 export default series(buildChecker, createPkg, publish);
